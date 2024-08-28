43d94d63f0130d48c1fbb9d0122acd6d6b7d62fc
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-08-28 11:12:27 +1000

    basics cpp

---
+++ basic/basic_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hi how are you doing!" << std::endl;               
+    std::cout << "I am doing great how about you..." << std::endl;
+
+    return 0;
+}
