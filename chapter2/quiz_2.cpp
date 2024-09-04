ba9e72e85b4478d0d5bcbf9c2d740f79fada4a22
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-09-04 10:50:36 +1000

    learn chap 2

---
+++ chapter2/quiz_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // for std::cout
+
+void doB()
+{
+    std::cout << "In doB()\n";
+}
+
+void doA()
+{
+    std::cout << "In doA()\n";
+
+    doB();
+}
+
+int main()
+{
+    std::cout << "Starting main()\n";
+
+    doA();
+    doB();
+
+    std::cout << "Ending main()\n";
+
+    return 0;
+}
