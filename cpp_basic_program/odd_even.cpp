ce7f0d8622f348acb6fe561792ca214c97892d4c
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-09-07 17:35:53 +1000

    finding that a number is odd or even

---
+++ cpp_basic_program/odd_even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+
+{
+    int x;
+    std::cout << "Enter an integer: " << '\n';
+    std::cin >> x ;
+
+    if (x % 2 == 0)
+    {
+       std::cout << x << " is even " << '\n'; 
+    }
+    else
+    std::cout << x << " is odd " << '\n';
+    
+}
