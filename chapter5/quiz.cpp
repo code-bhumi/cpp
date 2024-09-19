86e6115cee2a43444d1f7ddba5a2bcf4b696eeb9
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-09-19 12:29:08 +1000

    modified

--- chapter5/quiz.cpp
+++ chapter5/quiz.cpp
@@ -15,9 +15,11 @@ int main()
     
     std::cout << " Your name is " << name << " and you are " << age << " years old "<< '\n';
     
-    int letters{ static_cast<int>(name.length()) };
+    int num_letters{ static_cast<int>(name.length()) };
+
+    std::cout << " Numbers of letters in your Name are: "<< num_letters << '\n';
     
-    std::cout << " Sum of your age and numbers of letters in your name is " << letters + age << '\n';
+    std::cout << " Sum of your age and numbers of letters in your name is " << num_letters + age << '\n';
 
     return 0;
 }
